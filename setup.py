a4a7cff271b452dd17549c78ff8dd6d0c651cd66
Javier Duarte <jduarte@ucsd.edu>
2022-11-01 18:03:37 -0700

    turn on

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='p-clang-format',
-    version='1.0.2',
+    version='1.0.3',
     install_requires=['clang-format==14.0.6'],
     scripts=['p-clang-format']
 )
