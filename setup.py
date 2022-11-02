d2989c271196c790dceb74d559c807f90dace2b0
Javier Duarte <jduarte@ucsd.edu>
2022-11-01 17:50:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='p-clang-format',
-    version='1.0.1',
+    version='1.0.2',
     install_requires=['clang-format==14.0.6'],
     scripts=['p-clang-format']
 )
